2e7229622b47a230345cf2b83721717577657250
Константин <k.yazykov@bk.ru>
2023-02-10 00:34:31 +0300

    add: "Добавить новое поле friends"

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -8,16 +8,24 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @Builder
 public class User {
+    private final Set<Long> friends = new HashSet<>();
+    private Long id;
+
     private final LocalDate birthday;
     private String name;
     @NotBlank
     @NotNull
     private String login;
-    private int id;
+
+    public Set<Long> getFriends() {
+        return friends;
+    }
     @Email
     private String email;
 }
