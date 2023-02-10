69f84a7faf44e29a1956f14c79c815bf74c09366
Константин <k.yazykov@bk.ru>
2023-02-10 17:47:11 +0300

    add: "Добавлен лог об ошибке"

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler/ErrorHandler.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controller.ErrorHandler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -9,11 +10,13 @@ import ru.yandex.practicum.filmorate.model.ErrorResponse;
 
 
 @RestControllerAdvice
+@Slf4j
 public class ErrorHandler {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handlePostNotFoundException(final PostNotFoundException e) {
+        log.error("Ошибка, невозможно найти объект", e);
         return new ErrorResponse(
                 e.getMessage()
         );
